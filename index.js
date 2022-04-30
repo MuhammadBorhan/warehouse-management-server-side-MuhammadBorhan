501463743d7e81137e17f8ddcda6ae7965b62e23
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-04-30 19:58:57 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.48f58.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('wow connected')
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
-    res.send('Express connected');
+    res.send('md borhan uddin');
 })
 
 app.listen(port, () => {
