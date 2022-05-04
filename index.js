eb26a0678904e1aa20ba6173feadddb171ea7e08
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-04 23:14:00 +0600

    post and delete api created for myItems route

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("serviceProduct").collection("product");
+        const orderCollection = client.db("serviceProduct").collection("order");
 
         // Real all Product
         app.get('/product', async (req, res) => {
@@ -40,7 +41,7 @@ async function run() {
         });
 
 
-        // Update quantity
+        // Update quantity for add with previous quantity
         app.put('/product/:id', async (req, res) => {
             const id = req.params.id;
             const user = req.body;
@@ -49,11 +50,28 @@ async function run() {
             const updateDoc = {
                 $set: {
                     quantity: user.addQuantity,
+                    // quantity: (user.setQuantity),
                 },
             };
             const result = await productCollection.updateOne(filter, updateDoc, options);
             res.send(result);
-        })
+        });
+
+
+        // Update quantity reduce from quantity by deliver
+        // app.put('/product/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const data = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: {
+        //             quantity: data.setQuantity,
+        //         },
+        //     };
+        //     const result = await productCollection.updateOne(filter, updateDoc, options);
+        //     res.send(result);
+        // })
 
 
         // Delete single product
@@ -62,7 +80,34 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await productCollection.deleteOne(query);
             res.send(result);
-        })
+        });
+
+
+
+        // post myitems data
+        app.post('/order', async (req, res) => {
+            const newOrder = req.body;
+            const orders = await orderCollection.insertOne(newOrder);
+            res.send(orders);
+        });
+
+        // Read all product of single user in my items route
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const myItems = await cursor.toArray();
+            res.send(myItems);
+        });
+
+        // Delete single product
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 
