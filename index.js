5ee672d8d5f9afd5d9ddeb3600a69f3b89b348b8
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-05 16:47:12 +0600

    create get api for own email address

--- index.js
+++ index.js
@@ -14,35 +14,44 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const productCollection = client.db("serviceProduct").collection("product");
         const orderCollection = client.db("serviceProduct").collection("order");
 
-        // Real all Product
-        app.get('/product', async (req, res) => {
+        // post order 
+        app.post('/order', async (req, res) => {
+            const newOrder = req.body;
+            const orders = await orderCollection.insertOne(newOrder);
+            res.send(orders);
+        });
+
+
+        // Read all product 
+        app.get('/order', async (req, res) => {
             const query = {};
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
+            const cursor = orderCollection.find(query);
+            const myItems = await cursor.toArray();
+            res.send(myItems);
         });
 
         // Read single product
-        app.get('/product/:id', async (req, res) => {
+        app.get('/order/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const update = await productCollection.findOne(query);
+            const update = await orderCollection.findOne(query);
             res.send(update);
         });
 
-        // Post or Create single product
-        app.post('/product', async (req, res) => {
-            const newItem = req.body;
-            const result = await productCollection.insertOne(newItem);
-            res.send(result);
+        // Read all product of single user in my items route
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const myItems = await cursor.toArray();
+            res.send(myItems);
         });
 
 
         // Update quantity for add with previous quantity
-        app.put('/product/:id', async (req, res) => {
+        app.put('/order/:id', async (req, res) => {
             const id = req.params.id;
             const user = req.body;
             const filter = { _id: ObjectId(id) };
@@ -53,7 +62,7 @@ async function run() {
                     // quantity: (user.setQuantity),
                 },
             };
-            const result = await productCollection.updateOne(filter, updateDoc, options);
+            const result = await orderCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         });
 
@@ -73,32 +82,6 @@ async function run() {
         //     res.send(result);
         // })
 
-
-        // Delete single product
-        app.delete('/product/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await productCollection.deleteOne(query);
-            res.send(result);
-        });
-
-
-
-        // post myitems data
-        app.post('/order', async (req, res) => {
-            const newOrder = req.body;
-            const orders = await orderCollection.insertOne(newOrder);
-            res.send(orders);
-        });
-
-        // Read all product of single user in my items route
-        app.get('/order', async (req, res) => {
-            const email = req.query.email;
-            const query = { email: email };
-            const cursor = orderCollection.find(query);
-            const myItems = await cursor.toArray();
-            res.send(myItems);
-        });
 
         // Delete single product
         app.delete('/order/:id', async (req, res) => {
